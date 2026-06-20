66a751e0ab30ca1ff1c9886606305c1c0d646cfd
DiFargo <work.kriptonit.il@gmail.com>
2026-06-20 13:00:09 +0300

    refactor: extract nutrition food model helpers

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "workout-app-v640";
+const CACHE_NAME = "workout-app-v641";
 const WORKOUT_VIDEO_CACHE = "workout-videos-v2";
 const APP_SHELL = ["/", "/index.html", "/manifest.json"];
 
