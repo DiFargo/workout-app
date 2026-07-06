ee511eef0f52604860ecbb7e6acafa585470807b
DiFargo <work.kriptonit.il@gmail.com>
2026-07-06 05:07:34 +0300

    Split trainer nutrition diary CSS into layers

--- src/constants/appConfig.js
+++ src/constants/appConfig.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = "v.1.939";
+export const APP_VERSION = "v.1.940";
 export const BARCODE_SEARCH_ENABLED = false;
 export const INLINE_VIDEO_CONTROLS_HIDE_DELAY_MS = 850;
 export const STORAGE_KEY = "workout_tracker_v1";
